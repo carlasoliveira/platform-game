ea0ad1bba9d8d679d0054c5bb1e3cced363c1161
rSanches35 <rafaelsanches.s35@gmail.com>
2025-06-18 21:36:12 -0300

    Adição de sprite para objetos de Plataform

--- src/Platform.py
+++ src/Platform.py
@@ -15,22 +15,25 @@ class Vector:
 
 
 class Platform(ObjectStatic):
-    def __init__(self, position, size):
+    def __init__(self, position, size, image=None):
         super().__init__(position, size)
+        self.image = image
 
     def render(self, screen):
         pos = self.get_position()
-        size = self.get_size()
-        center = pos + (size / 2.0)
 
-        rect = pygame.Rect(
-            int(center.x - size.x / 2),
-            int(center.y - size.y / 2),
-            int(size.x),
-            int(size.y)
-        )
-
-        pygame.draw.rect(screen, (30, 30, 30), rect)
+        if self.image:
+            screen.blit(self.image, (int(pos.x), int(pos.y)))  # desenha a imagem
+        else:
+            size = self.get_size()
+            center = pos + (size / 2.0)
+            rect = pygame.Rect(
+                int(center.x - size.x / 2),
+                int(center.y - size.y / 2),
+                int(size.x),
+                int(size.y)
+            )
+            pygame.draw.rect(screen, (30, 30, 30), rect)  # fallback se não tiver imagem
 
     def __del__(self):
         pass
